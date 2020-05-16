9593532ab5d361ab28cabe51d24e6f2006e96ff7
Lucas Schmitt Oliveira <lucas.schmitt.oliveira@gmail.com>
2020-05-16 12:03:41 -0300

    buscando as oportunidades do usuario

--- back-end/v1/controllers/opportunityController.js
+++ back-end/v1/controllers/opportunityController.js
@@ -5,18 +5,18 @@ const authConfig = require('../config/auth.json');
 const jwt = require('jsonwebtoken');
 
 function verifyJWT(req, res, next){
+
     var token = req.headers['x-access-token'];
-    console.log(token)
+
     if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
     
     jwt.verify(token, authConfig.secret, function(err, decoded) {
-
-      console.log(err, decoded);
 
       if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
       
       req.ownerId = decoded.id;
       next();
+
     });
   }
 
@@ -38,15 +38,19 @@ router.post('/create', verifyJWT, async(req, res) => {
     }
 });
 
-router.get('/get', async(req, res) => {
+router.get('/get', verifyJWT, async(req, res) => {
 
     try{
         
-        // verificar como trata os query params
-        // retornar a lista baseada nos filtros
-        // se nao tiver, retorna os ultimos 50
+        let filter = {ownerId: req.ownerId};
 
- 
+        if(req.query.email)
+            filter.email = req.query.email;
+
+        let ret = await Opportunity.find(filter)
+        
+        res.send(ret);
+
     }catch(err){
         console.log(err)
         return res.status(400).send({ error: 'Falha no registro'});
